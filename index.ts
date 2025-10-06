d8052d4adb9cee67a8c67e0808750486a380d15a
Pro203S <prosirea2501@gmail.com>
2025-10-07 01:58:37 +0900

    os

--- index.ts
+++ index.ts
@@ -37,6 +37,7 @@ app.use("/os_logos", express.static(path.join(__dirname, "os_logos")));
 
 app.get("/", async (req, res) => {
     try {
+        const { scheme } = req.query;
         const cpu = await info.cpu();
         const mem = await info.mem();
         const os = await info.osInfo();
@@ -48,7 +49,23 @@ app.get("/", async (req, res) => {
                 "cores": cpu.cores
             },
             "mem": mem.total,
-            os
+            "os": {
+                "name": os.distro,
+                "release": os.release,
+                "logoUri": (() => {
+                    switch (os.logofile) {
+                        case "android": return "/os_logos/android.svg";
+                        case "apple": return scheme === "dark" ? "/os_logos/apple_dark.svg" : "/os_logos/apple_light.svg";
+                        case "macos": return scheme === "dark" ? "/os_logos/apple_dark.svg" : "/os_logos/apple_light.svg";
+                        case "debian": return "/os_logos/debian.svg";
+                        case "fedora": return "/os_logos/fedora.svg";
+                        case "ubuntu": return "/os_logos/ubuntu.svg";
+                        case "windows": return "/os_logos/windows.svg";
+                    }
+
+                    return "/os_logos/lunux.svg";
+                })()
+            }
         });
     } catch (err) {
         const e = err as Error;
